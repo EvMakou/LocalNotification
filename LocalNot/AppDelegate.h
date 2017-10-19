2681a022927897ec8f1179174f7c77c5d19766f6
supermacho <face_buk@mail.ru>
2017-10-20 01:20:46 +0300

    Initial Commit

---
+++ LocalNot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNot
+//
+//  Created by supermacho on 20.10.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalNot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalNot
+//
+//  Created by supermacho on 20.10.17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
